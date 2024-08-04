db6482a01fbd9d8599ec6253c10382e1cb9ccb25
Yonnji <yonnji@kitsune.one>
2024-08-04 09:11:04 +0300

    xrdb logging

--- config/qtile/qtilemods/tools/xrdb.py
+++ config/qtile/qtilemods/tools/xrdb.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 
+from libqtile.log_utils import logger
+
 XRESOURCES_PATH = os.path.expanduser('~/.Xresources')
 
 
@@ -8,4 +10,6 @@ def xrdb_merge(**opts):
     with open(XRESOURCES_PATH, 'w') as f:
         for k, v in opts.items():
             f.write(f'{k}: {v}\n')
-        subprocess.call(['xrdb', '-merge', XRESOURCES_PATH])
+    args = ['xrdb', '-merge', XRESOURCES_PATH]
+    logger.info(' '.join(args))
+    subprocess.call(args)
