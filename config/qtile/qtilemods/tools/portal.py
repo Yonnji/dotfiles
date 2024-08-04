2c642a4b85a89dedd2051780738f64a4e6ee424f
Yonnji <yonnji@kitsune.one>
2024-08-04 09:10:21 +0300

    portals updated

--- config/qtile/qtilemods/tools/portal.py
+++ config/qtile/qtilemods/tools/portal.py
@@ -2,25 +2,31 @@ import os
 import subprocess
 
 
-IS_X11 = os.getenv('XDG_SESSION_TYPE').lower() == 'x11'
 XDG_PORTALS = ['gnome', 'gtk', '']
-if IS_X11:
-    XDG_PORTALS.append('wlr')
+# XDG_PORTALS = ['kde', '']
 
 
-def portals_start():
+def portals_start(is_x11=False):
     env = dict(os.environ)
     env.update({
-        'XDG_SESSION_DESKTOP': 'gnome',
-        'XDG_CURRENT_DESKTOP': 'gnome',
+        'XDG_SESSION_DESKTOP': XDG_PORTALS[0],
+        'XDG_CURRENT_DESKTOP': XDG_PORTALS[0],
     })
 
-    for portal in XDG_PORTALS:
+    portals = copy.copy(XDG_PORTALS)
+    if is_x11:
+        portals.append('wlr')
+
+    for portal in portals:
         path = list(filter(None, ['/usr/libexec/xdg-desktop-portal', portal]))
         subprocess.Popen(['-'.join(path), '-vr'], start_new_session=True, env=env)
 
 
-def portals_stop():
+def portals_stop(is_x11=False):
+    portals = copy.copy(XDG_PORTALS)
+    if is_x11:
+        portals.append('wlr')
+
     for portal in XDG_PORTALS:
         name = list(filter(None, ['xdg-desktop-portal', portal]))
         subprocess.call(['pkill', '-'.join(name)])
